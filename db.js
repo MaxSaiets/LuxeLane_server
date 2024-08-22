39320fa52bc5b244f1f96fe7bc13280fd67564b9
Max Saiets <114159679+MaxSaiets@users.noreply.github.com>
2024-08-23 01:11:22 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const {Sequelize} = require('sequelize') // це Node.js ORM взаємодіяти з реляційними базами даних, такими як MySQL, PostgreSQL, SQLite
 
 module.exports = new Sequelize(
-    LuxeLane_postgreSQL,
-    root,
-    hwGTvoD9Usu6fGiLdMtiPtmmTwU2Xjk7,
+    "luxe_lanedb",
+    "root",
+    "hwGTvoD9Usu6fGiLdMtiPtmmTwU2Xjk7",
     {
         dialect: 'postgres',
-        host: 5432,
-        port: 5432,
+        host: "5432",
+        port: "5432",
         // logging: false // відключає логування запитів
     }
 )
