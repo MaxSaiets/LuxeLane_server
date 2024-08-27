9776dc49436a5eaa59c7b3750b2e833e1ab69db1
Max Saiets <114159679+MaxSaiets@users.noreply.github.com>
2024-08-28 01:50:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require('path')
 
 // Firebase
 const admin = require('firebase-admin');
-const serviceAccount = require('serviceAccountKey.json');
+const serviceAccount = require('./serviceAccountKey.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
