bc42a0404bd30dcc8abba00a2074633229d1c2f3
Max Saiets <114159679+MaxSaiets@users.noreply.github.com>
2024-10-23 20:01:40 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 const path = require('path')
 
 // FOR DEPLOY
-// const serviceAccountPath = path.resolve(__dirname, 'serviceAccountKey.json');
-// const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+const serviceAccountPath = path.resolve(__dirname, 'serviceAccountKey.json');
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
 
-const serviceAccount = require('./keys/serviceAccountKey.json');
+//const serviceAccount = require('./keys/serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
